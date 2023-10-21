96b649078977fe5d15d5d8e77480c3b14a5506d2
davidwmartines <d5172@yahoo.com>
2023-10-20 22:28:01 -0500

    Message list.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 from flask_sse import sse
 
 app = Flask(__name__)
 app.config["REDIS_URL"] = "redis://redis"
-app.register_blueprint(sse, url_prefix='/stream')
+app.register_blueprint(sse, url_prefix="/stream")
 
-@app.route('/')
+# in-memory list of all messages
+messages = []
+
+
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", messages=messages)
 
-@app.route('/hello')
-def publish_hello():
-    sse.publish({"message": "Hello!"}, type='greeting')
-    return "Message sent!"
+
+@app.route("/input", methods=["POST"])
+def receive_input():
+    messages.append(request.form["message"])
+    messages.append(f"reply to {request.form['message']}")
+    output = render_template("message_list.html", messages=messages)
+    sse.publish(output, type="message_list")
+    return Response("received", 201)
